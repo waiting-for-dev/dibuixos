a40d0ad64a945626195f2fbfbc09f92947d15a86
Marc Busqué <marc@lamarciana.com>
2023-06-25 16:44:03 +0200

    Prepare deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git push origin main
+git checkout source
+git merge main
+docker exec homemarccodedibuixos ruby index.rb > index.html
+git add index.html
+git commit -m "$(date +%s))"
+git push --set-upstream origin source
+git checkout main
