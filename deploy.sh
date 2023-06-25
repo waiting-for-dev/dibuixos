a11c665d5959035789ff93ebc49b9eba2ebaa6df
Marc Busqué <marc@lamarciana.com>
2023-06-25 17:04:05 +0200

    Fix commit message

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git checkout source
 git merge main
 docker exec homemarccodedibuixos ruby index.rb > index.html
 git add index.html
-git commit -m "$(date +%s))"
+git commit -m "$(date +%s)"
 git push --set-upstream origin source
 git checkout main
